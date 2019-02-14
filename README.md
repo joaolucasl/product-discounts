6c5ac04261ac20e0f69c195c94b233687649eb83
João Lucchetta <joao.lucas.lucchetta@gmail.com>
2019-02-14 10:29:15 -0200

    feat: add discount_rules table

--- README.md
+++ README.md
@@ -1,6 +1,15 @@
 # CHUMLEE
 > A multi-application service that manages Products and customised Discounts for different Users.
 
+
+## Running
+
+`docker-compose up mariadb`
+
+`docker-compose up discounts`
+
+`docker-compose up products`
+
 ## Architecture
 I'm trying to build this project with a DDD-like mindset. With that said, I'm trying to isolate the different _bounded contexts_ for each microservice. 
 
--- database/schema.sql
+++ database/schema.sql
@@ -12,14 +12,31 @@ CREATE TABLE `users` (
 	`first_name` VARCHAR(50) NOT NULL,
 	`last_name` VARCHAR(50) NOT NULL,
 	`date_of_birth` DATE NOT NULL,
-	PRIMARY KEY (`id`)
+	PRIMARY KEY (`id`),
+	UNIQUE KEY unique_external_id (external_id)
 );
 
-INSERT INTO `users` VALUES 
-	(NULL,'USR-010203040506','John','Doe','2000-10-31'),
-	(NULL,'USR-020304050607','Jane','Doe','1995-02-25'),
-	(NULL,'USR-030405060708','Forrest','Gump','2000-10-31'),
-	(NULL,'USR-040506070809','Wade','Wilson','1985-03-12');
+CREATE TABLE `discount_rules` (
+	`id` INT NOT NULL AUTO_INCREMENT,
+	`external_id` CHAR(16) NOT NULL,
+	`name` VARCHAR(100) NOT NULL,
+	`rule` TEXT NOT NULL,
+	`type` ENUM('MAX_DISCOUNT','DISCOUNT')
+)
 
-INSERT INTO `products` VALUES 
-	(NULL,'PRO-010203040506',10000,'','2000-10-31');
+INSERT INTO `discount_rules` VALUES
+	(NULL, 'RUL-AAAAAAAAAAAA', '5% discount on User birthday', 'const now = new Date(); const sameDay = user.date_of_birth.getDate() == now.getDate(); const sameMonth = user.date_of_birth.getMonth() == now.getMonth() ; if (sameDay && sameMonth) { discount = 0.05; }', 'DISCOUNT'),
+	(NULL, 'RUL-BBBBBBBBBBBB', '10% discount on 2019 Black Friday', 'const now = new Date(); if (now.getTime() == new Date(2019, 11, 25).getTime() ) { discount = 0.1; }', 'DISCOUNT'),
+	(NULL, 'RUL-BBBBBBBBBBBB', 'Max Discount of 10%', 'discount = 0.1', 'MAX_DISCOUNT');
+
+INSERT INTO `users` VALUES 
+	(NULL,'USR-AAAAAAAAAAAA','John','Doe','2000-10-31'),
+	(NULL,'USR-BBBBBBBBBBBB','Jane','Doe','1995-02-25'),
+	(NULL,'USR-CCCCCCCCCCCC','Forrest','Gump','2000-10-31'),
+	(NULL,'USR-DDDDDDDDDDDD','Wade','Wilson','1985-03-12');
+
+INSERT INTO `products` VALUES
+	(NULL,'PRO-AAAAAAAAAAAA',300000,'iPhone 8','An overpriced phone with a fanbase'),
+	(NULL,'PRO-BBBBBBBBBBBB',200000,'Xiaomi Mi 8','An underpriced phone with a fanbase'),
+	(NULL,'PRO-CCCCCCCCCCCC',10000,'Nokia','An very resistent phone made of adamantium'),
+	(NULL,'PRO-DDDDDDDDDDDD',50000,'AirPods','Wireless earbuds for twice the price. Nice!');
