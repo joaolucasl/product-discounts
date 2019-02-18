0dfb4c4c049ebb48ea35afa8f6a1becd10fc6935
João Lucchetta <joao.lucas.lucchetta@gmail.com>
2019-02-18 09:12:44 -0300

    fix(products): GET /products returning jSON

--- products/src/main/kotlin/com/github/joaolucasl/chumlee/products/interfaces/controllers/ProductsController.kt
+++ products/src/main/kotlin/com/github/joaolucasl/chumlee/products/interfaces/controllers/ProductsController.kt
@@ -3,6 +3,8 @@ package com.github.joaolucasl.chumlee.products.interfaces.controllers
 import com.github.joaolucasl.chumlee.products.application.services.ProductsService
 import com.github.joaolucasl.chumlee.products.infrastructure.transformers.JsonTransformer
 import org.koin.spark.SparkController
+import spark.Request
+import spark.Response
 import spark.Route
 import spark.Spark.path
 import spark.Spark.get
@@ -14,7 +16,9 @@ class ProductsController(
 
     init {
         path("/products") {
-            get("/", Route { productsService.listProducts() }, jsonTransformer)
+            get("", "application/json", Route { _, _ ->
+                ProductsListDTO(productsService.listProducts())
+            }, jsonTransformer)
         }
     }
 }
---
+++ products/src/main/kotlin/com/github/joaolucasl/chumlee/products/interfaces/controllers/ProductsListDTO.kt
@@ -0,0 +1,5 @@
+package com.github.joaolucasl.chumlee.products.interfaces.controllers
+
+import com.github.joaolucasl.chumlee.products.domain.Product
+
+data class ProductsListDTO(val products: List<Product>)
